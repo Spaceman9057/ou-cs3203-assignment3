21379e9fdf833ed2ab639f6a33a879902b3a0255
Isaac Stein <spaceman9057@gmail.com>
2023-02-14 17:52:53 -0600

    listSummer added

--- ListModder.cpp
+++ ListModder.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <vector>
 
+int listSummer(vector <int> list) {
+	sum = 0;
+
+	for (int i = 0; int < list.size(); i++) {
+		sum += list[i];
+	}
+
+	return sum;
+}
+
 int main {
 
 }
