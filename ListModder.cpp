e0b7076871c7c02081e974e674ce4430a11325ce
Isaac Stein <spaceman9057@gmail.com>
2023-02-14 18:11:59 -0600

    added main function. Might have made a slight mistake with the commits, forgot to add something. Whoops.

--- ListModder.cpp
+++ ListModder.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
 int listSummer(vector <int> list) {
-	sum = 0;
+	int sum = 0;
 
 	for (int i = 0; int < list.size(); i++) {
 		sum += list[i];
@@ -11,6 +13,27 @@ int listSummer(vector <int> list) {
 	return sum;
 }
 
-int main {
+int listProduct(vector <int> list) {
+	int product = 1;
 
+	for (int i = 0; int < list.size(); i++) {
+		product *= list[i];
+	}
+
+	return product;
 }
+
+int main{
+	vector<int> InputList;
+	int temp;
+	CountSort x;
+	RadixSort y;
+
+	// takie inputs
+	while (cin >> temp) {
+		InputList.push_back(temp);
+	}
+
+	cout << listSummer(InputList);
+	cout << listProduct(InputList);
+ }
